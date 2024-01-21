a2af3a35f0eff9b73fd61652872880a5a3e4e516
Ove Sundal <osundal83@gmail.com>
2024-01-21 22:08:13 +0100

    add tracing with langfuse

--- app/chat/chains/retrieval.py
+++ app/chat/chains/retrieval.py
@@ -1,7 +1,9 @@
 from langchain.chains import ConversationalRetrievalChain
 from app.chat.chains.streamable import StreamableChain
+from app.chat.chains.traceable import TraceableChain
 
 
 # mixin
-class StreamingConversationalRetrievalChain(StreamableChain, ConversationalRetrievalChain):
+class StreamingConversationalRetrievalChain(
+    TraceableChain, StreamableChain, ConversationalRetrievalChain):
     pass
